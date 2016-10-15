baa95695e64930ac97e17f45be7fd944ad90005e
Koty Wong <kotywong@Kotys-MacBook-Pro.local>
2016-10-15 11:31:47 -0400

    adding bundler

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,5 +1,11 @@
 from flask import Flask, render_template
+from flask_assets import Environment, Bundle
+
 app = Flask(__name__)
+assets = Environment(app)
+
+js = Bundle('./js/scripts.js', output='gen/packed.js')
+assets.register('js_all', js)
 
 import calculator.views
 
