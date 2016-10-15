d9d88a273940f8844fe78610e12cb4a9837c6fa4
Koty Wong <kotywong@Kotys-MacBook-Pro.local>
2016-10-15 14:47:54 -0400

    styles adding functions

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -2,10 +2,15 @@ from flask import Flask, render_template
 from flask_assets import Environment, Bundle
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
+
 assets = Environment(app)
 
 js = Bundle('./js/scripts.js', output='gen/packed.js')
 assets.register('js_all', js)
+
+scss = Bundle('./scss/styles.scss', filters='pyscss', output='gen/packed.css')
+assets.register('scss_all', scss)
 
 import calculator.views
 
--- calculator/static/js/scripts.js
+++ calculator/static/js/scripts.js
@@ -2,10 +2,132 @@ console.log('herro')
 
 $(document).ready(function() {
 
+  var augend = 0; 
+  var addend = 0;
+  var answer = 0;
+  var theRealAnswer = 0;
+  var operation = "";
   // number functions
   $('#one').click(function() {
-    alert("handler has been clicked")
-  })
+    // alert("handler has been clicked")
+    $('#window').empty()
+    augend = 1;
+    // augend = ($('#window').html())
+    $('#window').append(augend)
+    console.log(augend)
+    operation = "";
+  });
+
+  $('#two').click(function() {
+    $('#window').empty()
+    augend = 2;
+    $('#window').append(augend)
+    console.log(augend)
+    operation = "";
+  });
+
+  $('#plus').click(function() {
+  operation = "plus";
+  if (operation === "plus") {
+    answer += augend
+    $('#window').empty();
+    $('#window').append(answer);
+    console.log(answer);
+    augend = 0;
+  }
+
+  });
+
+  $('#minus').click(function() {
+  operation = "minus";
+  if (operation  === "minus") {
+    answer -= augend
+    $('#window').empty()
+    $('#window').append(answer)
+    console.log(answer)
+    augend = 0;
+  }
+
+  });
+
+  $('#three').click(function(){
+    $('#window').empty()
+    augend = 3;
+    $('#window').append(augend)
+    console.log(augend)
+  });
+
+  $('#four').click(function(){
+    $('#window').empty()
+    augend = 4;
+    $('#window').append(augend)
+    console.log(augend)
+  });
+
+  $('#five').click(function(){
+    $('#window').empty()
+    augend = 5;
+    $('#window').append(augend)
+    console.log(augend)
+  });
+
+  $('#six').click(function(){
+    $('#window').empty()
+    augend = 6;
+    $('#window').append(augend)
+    console.log(augend)
+  });
+
+  $('#seven').click(function(){
+    $('#window').empty()
+    augend = 7;
+    $('#window').append(augend)
+    console.log(augend)
+  });
+
+  $('#eight').click(function(){
+    $('#window').empty()
+    augend = 8;
+    $('#window').append(augend)
+    console.log(augend)
+  });
+
+  $('#nine').click(function(){
+    $('#window').empty()
+    augend = 9;
+    $('#window').append(augend)
+    console.log(augend)
+  });
+
+  $('#zero').click(function(){
+    $('#window').empty()
+    augend = 0;
+    $('#window').append(augend)
+    console.log(augend)
+  });
+
+
+  $('#equals').click(function() {
+    if (operation === "plus") {
+      answer += augend;
+    } else if (operation === "minus") {
+      answer -= augend;
+    }
+    $('#window').empty()
+    $('#window').append(answer)
+    // answer.push(augend)
+    // answer.forEach(function(x){
+    //   theRealAnswer += x;
+    // })
+    // console.log(theRealAnswer)
+  });
+
+
+  $('#clear').click(function() {
+    $("#window").empty()
+    answer = 0;
+    augend = 0;
+  });
 
 
 
